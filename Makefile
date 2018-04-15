e6042c30fcfcce8ac62d547fe4bda6d93f63a836
lanieri <lanieri@eurecom.fr>
2018-04-15 12:12:51 +0200

    Updated Makefile and lab2.c

--- Makefile
+++ Makefile
@@ -1,6 +1,6 @@
 CC = gcc
 
-#-mavx is for AVX2 ISA, -marc=native forces the compiler to use available ISA to compile, -msse4.1 is apparently more Linux friendly/up to date, march however should be knl, haswell or sandybridge
+#-mavx is for AVX2 ISA, -marc=native forces the compiler to use available the available ISA instructions to compile. In order to be able to compile AVX2 instructions, however, march should be knl, haswell or sandybridge. -msse4.1 is apparently more Linux friendly/up to date.
 CFLAGS = -Wall -march=haswell -mmmx -msse -msse2 -mssse3 -msse4 -msse4.1 -mavx
 
 # Utility routines
@@ -8,10 +8,10 @@ UTIL_OBJS += lab2.o
 OBJ = $(UTIL_OBJS)
 
 all: $(OBJ)
-	$(CC) $(CFLAGS) $(OBJ) -o lab2f
+	$(CC) $(CFLAGS) $(OBJ) -o lab2
 
 %.o : %.c
-	$(CC) -c $(CFLAGS) -Ofast $< 
+	$(CC) -c $(CFLAGS) $< 
 
 clean:
 	rm -f *.o 
--- lab2.c
+++ lab2.c
@@ -1,4 +1,8 @@
-// Include files for SSE4
+// Compmeth - assignment 2
+// Valerio Lanieri, Eurecom, Promo 2019
+// Repository: https://github.com/cold-ice/compmethlanieri2
+
+// Include headers for SSE4 
 #include <emmintrin.h>
 #include <xmmintrin.h>
 #include <tmmintrin.h>
@@ -30,18 +34,18 @@ inline __m128i cpack(__m128i xre,__m128i xim) {
 	cpack_tmp2 = _mm_srai_epi32(cpack_tmp2,15);
 	return(_mm_packs_epi32(cpack_tmp1,cpack_tmp2));
 
-}
-*/
-// routines to be written
+}*/
+
 void componentwise_multiply_real_scalar(int16_t *x,int16_t *y,int16_t *z,uint16_t N) {
 	int i;
 	for(i=0; i<N; i++){
 		z[i]=(int16_t) ( ( (int32_t)x[i]* (int32_t)y[i] ) >> 15);
-		//printf("%hu %d\n", i, z[i]);
+		#ifdef DEBUG
+			printf("%hu %d\n", i, z[i]);
+		#endif
 	}
 }
 
-// routines to be written
 void componentwise_multiply_real_sse4(int16_t *x, int16_t *y, int16_t *z, uint16_t N) {
 	uint16_t i;
 	for(i=0; i<N; i+=8){
@@ -49,18 +53,27 @@ void componentwise_multiply_real_sse4(int16_t *x, int16_t *y, int16_t *z, uint16
 		__m128i *y128 = (__m128i *)&y[i];
 		__m128i *z128 = (__m128i *)&z[i];
  		*z128 = _mm_mulhrs_epi16(*x128, *y128);
-		//printf("%hu %d\n", i/8, z[i]);
+		#ifdef DEBUG
+			printf("%hu %d\n", i/8, z[i]);
+		#endif
 	}
 }
 
 void componentwise_multiply_real_avx2(int16_t *x, int16_t *y, int16_t *z, uint16_t N) {
 	uint16_t i;
 	for(i=0; i<N; i+=16){
-		__m256i x256 = _mm256_loadu_si256((__m256i *)&x[i]);
+		// The following, commented code was used before knowing of the existence of the aligned_alloc facility. The new version, in principle, allows to better compare the performance of the AVX2 implementation with respect to SSE4 since the setup of the componentwise_multiply_real functions are identical, with the exception of the data type.
+		/*__m256i x256 = _mm256_loadu_si256((__m256i *)&x[i]);
 		__m256i y256 = _mm256_loadu_si256((__m256i *)&y[i]);
  		x256 = _mm256_mulhrs_epi16(x256, y256);
-		_mm256_storeu_si256((__m256i *)&z[i], x256);
-		//printf("%hu %d\n", i/16, z[i]);
+		_mm256_storeu_si256((__m256i *)&z[i], x256);*/
+		__m256i *x256 = (__m256i *)&x[i];
+		__m256i *y256 = (__m256i *)&y[i];
+		__m256i *z256 = (__m256i *)&z[i];
+ 		*z256 = _mm256_mulhrs_epi16(*x256, *y256);
+		#ifdef DEBUG
+			printf("%hu %d\n", i/16, z[i]);
+		#endif
 	}
 }
 
@@ -71,10 +84,13 @@ void componentwise_multiply_real_avx2_opt(int16_t *x, int16_t *y, int16_t *z, ui
 		__m256i *y256 = (__m256i *)&y[i];
 		__m256i *z256 = (__m256i *)&z[i];
  		*z256 = _mm256_mulhrs_epi16(*x256, *y256);
-		//printf("%hu %d\n", i/8, z[i]);
+		#ifdef DEBUG
+			printf("%hu %d\n", i/8, z[i]);
+		#endif
 	}
 }
 
+// Not yet tested as it is not possible to compile the following function without without the appropriate architecture
 /*void componentwise_multiply_real_avx512(int16_t *x, int16_t *y, int16_t *z, uint16_t N) {
 	uint16_t i;
 	for(i=0; i<N; i+=32){
@@ -93,7 +109,7 @@ int main(int argc, char* argv[]) {
 	time_stats_t ts;
 
 	if(argc!=4){
-		printf("Arugments: architecture(0=scalar, 1=sse4, 2=avx2, 3=avx512) array_length number_of_tests\n");
+		printf("Arugments: architecture(0=scalar, 1=sse4, 2=avx2) array_length (must be a multiple of 16) number_of_tests\n");
    	abort();
 	}
 
@@ -109,43 +125,61 @@ int main(int argc, char* argv[]) {
 		snprintf(archs, 6, "AVX2");
 	}
 
+	// Given my implementation of the tests the vector size has to be a multiple of 16, otherwise the AVX2 implementation won't have enough input elements at each cycle. Notice that this limitation could have been easily solved by doing N%16, adding the remainder to N and forcing the extra array elements to 0. By the same principle, the SSE4 issue would have been taken care of in a similar manner. This however was not done since it didn't serve any particularly useful purpose. Moreover, in order to better compare the various architectures, it made more sense to properly fill all the vector elements with randomly generated numbers instead of zeros.
 	N=atoi(argv[2]);
 	if(N%16!=0) {
-		printf("Inappropriate vector size, must be a multiple of 8 for sse4, a multiple of 16 for avx2 or a multiple of 32 for avx512.\n");
+		printf("Inappropriate vector size, must be a multiple of 16.\n");
 		abort();
 	}
+
+	//alligned_alloc was used instead of malloc since otherwise the vector elements would not be properly distantiated for the heap when using the AVX2 architecture, causing a segmentation fault. Notice that N*sizeof(int16_t) has to be a multpiple of 32. This constraint is automatically satisfied since I have imposed that N has to be a multiple of 16, and 16*16=256 is already a multiple of 32.
 	xv=aligned_alloc(32, N*sizeof(int16_t));	
 	yv=aligned_alloc(32, N*sizeof(int16_t));
 	zv=aligned_alloc(32, N*sizeof(int16_t));
 
+	// Since one test per architecture wouldn't be enough to assess performances in a satisfactory way, multiple tests are to be performed when using a certain vector length. The overall timestamp is then divided by the number of performed tests in order to have an average execution time.
 	Ntest=atoi(argv[3]);
 
 	FILE *x, *y, *z;
 
+	// Open input file 1 (X) and instantiate first vector
 	x=fopen("X", "r");
 	if(x==NULL){
 		printf("X not found\n");
    	abort();
 	}
 	i=0;
-	//printf("X:\n");
+	#ifdef DEBUG
+		printf("X:\n");
+	#endif
 	while(fscanf(x, "%hi", &xv[i])!=0 && i<N){
-		//printf("%hi\n", xv[i]);
+		#ifdef DEBUG
+			printf("%hi\n", xv[i]);
+		#endif
 		i++;
 	}
 
-	//printf("Y:\n");
+	// Open input file 2 (Y) and instantiate second vector
 	y=fopen("Y", "r");
 	if(y==NULL){
 		printf("Y not found\n");
    	abort();
 	}
 	i=0;
+	#ifdef DEBUG
+		printf("Y:\n");
+	#endif
 	while(fscanf(y, "%hi", &yv[i])!=0 && i<N){
-		//printf("%hi\n", yv[i]);
+		#ifdef DEBUG
+			printf("%hi\n", yv[i]);
+		#endif
 		i++;
 	}
-	//printf("Z:\n");
+
+	#ifdef DEBUG
+		printf("Z:\n");
+	#endif
+
 	reset_meas(&ts);
 	start_meas(&ts);
 	for(i=0; i<Ntest; i++){
@@ -158,27 +192,29 @@ int main(int argc, char* argv[]) {
 				break;
 			case 2:
 				componentwise_multiply_real_avx2_opt(xv, yv, zv, N);
-				break;
-			/*case 3:
-				componentwise_multiply_real_avx512(xv, yv, zv, N);
-				break;*/
+	 			break;
 			default:
 				printf("Invalid argument for architecture: %d. It must be between 0 and 2.\n", arch);
 				break;
 		}
 	}
 	stop_meas(&ts);
-	printf("%s\t%hu\t%lld\t%lld\n", archs, N, ts.diff/Ntest, ts.max);
+	printf("%s %hu %lld %lld\n", archs, N, ts.diff/Ntest, ts.max);
+
+	// Write results in the output file Z. This is rather useless for testing purposes, however this allowed to assess the correct functionality of the SSE4 and AVX2 functions.
 	z=fopen("Z", "w");
 	if(z==NULL){
 		printf("Couldn't create Z\n");
    	abort();
 	}
 	i=0;
-	while(fprintf(z, "%hi\n", zv[i])!=0 && i<N){
+	while(i<N && fprintf(z, "%hi\n", zv[i])!=0){
 		i++;
 	}
 
+	free(xv);
+	free(yv);
+	free(zv);
 	fclose(x);
 	fclose(y);
 	fclose(z);
